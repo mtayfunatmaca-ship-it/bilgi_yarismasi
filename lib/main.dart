e6456670aded81d1abeef7915cccef690f0510a0
mtayfunatmaca-ship-it <m.tayfunatmaca@gmail.com>
2025-10-30 12:57:31 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:bilgi_yarismasi/screens/auth_wrapper.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:provider/provider.dart'; 
+import 'package:provider/provider.dart';
 import 'package:bilgi_yarismasi/services/theme_notifier.dart';
 import 'package:bilgi_yarismasi/services/auth_service.dart';
 import 'package:bilgi_yarismasi/services/user_data_provider.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart'; 
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:bilgi_yarismasi/services/ad_service.dart';
 import 'package:bilgi_yarismasi/services/purchase_service.dart';
 
@@ -18,7 +18,7 @@ void main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await initializeDateFormatting('tr_TR', null);
   await MobileAds.instance.initialize();
-  
+
   // Servisleri başlat
   final AuthService authService = AuthService();
   final AdService adService = AdService();
@@ -26,29 +26,28 @@ void main() async {
   final PurchaseService purchaseService = PurchaseService();
 
   // Veri yükleyici (Yorumda olduğundan emin ol)
-  //final uploader = FirebaseDataUploader(); 
-  //await uploader.uploadDataFromJson();
-  
+  final uploader = FirebaseDataUploader();
+  await uploader.uploadDataFromJson();
+
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(
-          create: (context) => ThemeNotifier(),
-        ),
+        ChangeNotifierProvider(create: (context) => ThemeNotifier()),
         ChangeNotifierProvider(
           create: (context) => UserDataProvider(authService),
         ),
-        Provider<AuthService>( // AuthService 'ChangeNotifier' değil, o yüzden 'Provider' kalmalı
+        Provider<AuthService>(
+          // AuthService 'ChangeNotifier' değil, o yüzden 'Provider' kalmalı
           create: (_) => authService,
         ),
-        
+
         // --- DÜZELTME BURADA ---
         // 'dispose:' parametresi kaldırıldı, çünkü ChangeNotifierProvider bunu otomatik yapar.
         ChangeNotifierProvider<AdService>(
           create: (_) => adService,
           // dispose: (_, adService) => adService.dispose(), // <<< BU SATIR HATA VERİYORDU, KALDIRILDI
         ),
-        
+
         // 'dispose:' parametresi kaldırıldı
         ChangeNotifierProvider<PurchaseService>(
           create: (_) => purchaseService,
